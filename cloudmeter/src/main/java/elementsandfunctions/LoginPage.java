77bae3932b7ed46d7a71d3b2037e3cf47a253f19
Deepak-merahkee <34372274+Deepak-merahkee@users.noreply.github.com>
2018-09-21 20:52:28 +0530

    Add files via upload

---
+++ cloudmeter/src/main/java/elementsandfunctions/LoginPage.java
@@ -0,0 +1,21 @@
+package elementsandfunctions;
+
+/**
+ * Hello world!
+ *
+ */
+public class LoginPage
+{
+	public static String lauch="mammoth";
+	public static String clikced="Loging to mammoth";
+	
+	public static String lauchapp()
+	{
+		return ("Returning from lauch app "+ lauch);
+	}
+	
+	public static String ClickApp()
+	{
+		return ("Returning from click app "+ clikced);
+	}
+}
---
+++ cloudmeter/src/test/java/merahkee/cloudmeter/MammothTest.java
@@ -0,0 +1,32 @@
+package merahkee.cloudmeter;
+
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.*;
+import elementsandfunctions.LoginPage;
+
+/**
+ * Unit test for simple App.
+ */
+public class MammothTest 
+{
+	
+	@BeforeTest
+	public void lauchApp()
+	{
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Deepak\\Documents\\apache-jmeter-3.3\\apache-jmeter-3.3\\bin\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\Documents\\apache-jmeter-3.3\\apache-jmeter-3.3\\bin\\chromedriver.exe");
+       // WebDriver driver = new ChromeDriver();
+		System.out.println(LoginPage.lauchapp());
+	}
+	
+	@Test 
+	public void clickbutton()
+	{
+		System.out.println(LoginPage.ClickApp());
+	}
+
+}

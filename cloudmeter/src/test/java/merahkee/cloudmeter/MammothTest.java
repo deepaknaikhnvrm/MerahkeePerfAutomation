27e9c836be28cc346c9d0019f8b2074961d30275
Yajana <yajana.merahkee@gmail.com>
2018-10-04 19:09:44 +0530

    Updated assertion

--- cloudmeter/src/test/java/merahkee/cloudmeter/MammothTest.java
+++ cloudmeter/src/test/java/merahkee/cloudmeter/MammothTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.*;
 import elementsandfunctions.LoginPage;
+import org.testng.*;
 
 /**
  * Unit test for simple App.
@@ -21,12 +22,14 @@ public class MammothTest
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\Documents\\apache-jmeter-3.3\\apache-jmeter-3.3\\bin\\chromedriver.exe");
        // WebDriver driver = new ChromeDriver();
 		System.out.println(LoginPage.lauchapp());
+		Assert.assertEquals(LoginPage.lauchapp(),"Returning from lauch app mammoth");
 	}
 	
 	@Test 
 	public void clickbutton()
 	{
 		System.out.println(LoginPage.ClickApp());
+		Assert.assertEquals(LoginPage.ClickApp(),"Returning from click app Loging to mammoth");
 	}
 
 }
